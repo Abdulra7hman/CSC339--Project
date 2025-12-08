989506ccbb077ed36800c396f39ba2c17986bb45
Pb22j <151536650+Pb22j@users.noreply.github.com>
2025-12-08 18:22:53 +0300

    Update State.java

--- Pb22j/State.java
+++ Pb22j/State.java
@@ -1,17 +1,22 @@
 public class State {
     public String name;
-    public State next;
+    public State[] next;
     public Alphabet alphabet;
-    public State(String name, Alphabet alphabet){
+    public String IsFinal; //to See if The State is Fianl or not
+    public State(String name, Alphabet alphabets){
+  
         this.name=name;
-        this.alphabet=alphabet;
+        next= new State[alphabets.ListOfChar.length];
+        this.alphabet=alphabets; 
     }
+
     public boolean addState(char a, State next){
-        
-        if(!alphabet.inAlphabet(a)){
-            System.out.println(a +" Is Not in the Alphabet");    
-            return false;
-        }      
+        int indexAlphabet=alphabet.inAlphabet(a);
+            if(indexAlphabet==-1){
+                System.out.println(a+" not In the Alphabet");
+                return false;
+            }
+            this.next[indexAlphabet]=next;
         return true;
     }
 }
